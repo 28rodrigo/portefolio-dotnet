f4c9e1d6b1e95049264b2c531dad971e8a3ed31c
Rodrigo Pereira <28rodrigopereira@gmail.com>
2023-07-10 22:30:58 +0100

    add new info

---
+++ src/BlazorApp/Models/Article.cs
@@ -0,0 +1,7 @@
+namespace BlazorApp.Models;
+public class Article
+{
+    public string Title { get; set; } = string.Empty;
+    public string Authors { get; set; } = string.Empty;
+    public string Link { get; set; } = string.Empty;
+}
